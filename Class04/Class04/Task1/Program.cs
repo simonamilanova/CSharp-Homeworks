143511919a8e9375af03ef6d8919c8ac7ee433c7
simonamilanova <smilanova97@gmail.com>
2021-04-07 22:15:05 +0200

    New Homework

---
+++ Class04/Class04/Task1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentance");
+            string userInput = Console.ReadLine();
+
+            FunWithStrings(userInput);
+        }
+
+        static void FunWithStrings(string str)
+        {
+            //reverse string
+            for (int n = str.Length - 1; n >= 0; n--)
+            {
+                Console.WriteLine(str[n]);
+            }
+
+            //total number of vowels
+            int i = 0;
+            foreach (char character in str)
+            {
+                str.ToLower().ToCharArray();
+
+                if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u')
+                {
+                    i++;
+
+                }
+
+            }
+
+            //is string palindrome 
+            string palindrome = "";
+            for (int counter = str.Length - 1; counter > -1; counter--)
+            {
+                palindrome += str[counter];
+            }
+
+            if (palindrome.ToLower() == str.ToLower())
+            {
+                palindrome = "palindrome";
+            }
+            else
+            {
+                palindrome = "not a palindrome";
+            }
+
+            ////find the largest and smallest word 
+            string largestWord = "";
+            string smallestWord = "";
+            foreach (string word in str.Split())
+            {
+                if (word.Length > largestWord.Length)
+                {
+                    largestWord = word;
+                }
+
+                if(word.Length < smallestWord.Length)
+                {
+                    smallestWord = word;
+                }
+            }
+
+            //find the number of words
+            int countWord = 0;
+            int numOfWords = 1;
+
+            while (countWord <= str.Length - 1)
+            {
+                if (str[countWord] == ' ')
+                {
+                    numOfWords++;
+                }
+
+                countWord++;
+            }
+
+            Console.WriteLine($"The number of vowels in the string is {i}, the string is {palindrome}");
+            Console.WriteLine($"The number of words is {numOfWords}");
+            Console.WriteLine($"the largest word is {largestWord} and the smallest word is {smallestWord}");
+    
+        }
+    }
+}
---
+++ Class04/Class04/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string word = "The&nbsp;&nbsp;&nbsp; best &nbsp;Lorem&nbsp; Ipsum&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Generator in all the&nbsp; sea!&nbsp;&nbsp; Heave this &nbsp; scurvy copyfiller fer yar&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; next&nbsp;&nbsp; adventure&nbsp; and cajol yar clients&nbsp;&nbsp; into walking  the plank with  ev'ry layout!&nbsp;&nbsp;&nbsp; Configure&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  above, then get yer pirate ipsum...own the high seas,&nbsp;&nbsp;&nbsp; argh!";
+            string word2 = "    Hello there";   
+            RemoveExtraSpaces(word2);
+
+        }
+
+        static void RemoveExtraSpaces (string str)
+        {
+            string word = str.Trim();
+            Console.WriteLine(word);
+        }
+    }
+}
